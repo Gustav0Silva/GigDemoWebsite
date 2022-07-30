44c0d4b8b60e9c0b1f3fb47be9c45e7add769a60
Siphos <gustavo.fm.silva@gmail.com>
2022-07-30 22:54:52 +0100

    Solving DB issues

--- app/server.js
+++ app/server.js
@@ -24,10 +24,10 @@ app.get('/profile-picture', function (req, res) {
 let mongoUrlLocal = "mongodb://admin:password@localhost:27017";
 
 // use when starting application as a separate docker container
-let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
+// let mongoUrlDocker = "mongodb://admin:password@host.docker.internal:27017";
 
 // use when starting application as docker container, part of docker-compose
-let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
+//let mongoUrlDockerCompose = "mongodb://admin:password@mongodb";
 
 // pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
